e1eb7e8c54c45ca7b5e2945dafd740e0454c7eb8
quickplugin <avijitpaul03021992@gmail.com>
2022-07-17 11:51:12 +0530

    pwa chnages

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,23 @@ import { Fragment, useState } from "react";
 import Head from 'next/head';
 import GlobalStyle from '../styles/globals/globalStyle.style';
 import PageContainerWrapper from '../components/PageContainerWrapper/PageContainerWrapper';
-import ServicingContext from "../context/context"; 
+import ServicingContext from "../context/context";
 
 export default function Home() {
-    const [show, setShowBackdrop] = useState(false);
-    const value = { show, setShowBackdrop };
+  const [show, setShowBackdrop] = useState(false);
+  const value = { show, setShowBackdrop };
   return (
     <>
-    <ServicingContext.Provider value={value}>
-      <Head>
-        <meta name='application-name' content='My Digital Wallet' />
-        <title>My Bookmark</title>
-      </Head>
-      <GlobalStyle />
-      <PageContainerWrapper>
-        <p>Hello World</p>
-      </PageContainerWrapper>
+      <ServicingContext.Provider value={value}>
+        <Head>
+          <meta name='manifest' content='/manifest.json' />
+          <meta name='application-name' content='My Digital Wallet' />
+          <title>My Bookmark</title>
+        </Head>
+        <GlobalStyle />
+        <PageContainerWrapper>
+          <p>Hello World</p>
+        </PageContainerWrapper>
       </ServicingContext.Provider>
     </>
   );
