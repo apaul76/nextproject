e728bae8a3b2fc8286bbac4799a02b4bd270a0fb
quickplugin <avijitpaul03021992@gmail.com>
2022-07-24 23:00:23 +0530

    new changes

--- styles/components/PersonalCards.style.js
+++ styles/components/PersonalCards.style.js
@@ -8,9 +8,6 @@ background: #ffff;
 border-radius: 5px;
 height: 200px;
 width: 100%;
-color: black;
-align-items: center;
-justify-content: center;
 `
 
 export {
---
+++ styles/globals/globalStyle.style.js
@@ -0,0 +1,17 @@
+import styled, {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle `
+    body{
+        margin:0px;
+        background: #1f2d7c;
+        color:#ffff;
+        & #simple-popover{
+            & .MuiPopover-paper{
+                background:#ffff!important;
+                overflow:unset!important;
+                border:1px solid white;
+            }
+        }
+    }
+`
+export default GlobalStyle
