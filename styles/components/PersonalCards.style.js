dcb76f1036795484bd47e50be78d950d9aee0899
quickplugin <avijitpaul03021992@gmail.com>
2022-07-26 21:47:35 +0530

    cards changes pattern

--- styles/components/PersonalCards.style.js
+++ styles/components/PersonalCards.style.js
@@ -14,7 +14,7 @@ background-color: ${(props) => colorPicker[props.colorScheme]};
 color: rgba(0, 0, 0, 0.87);
 transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
 border-radius: 10px;
-box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
+box-shadow: rgb(0 0 0 / 20%) 1px 3px 7px 4px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
 overflow: hidden;
 width: 100%;
 height:200px;
