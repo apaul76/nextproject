ea91c52f7f77110c8ce6ecc1a923c896470d9c19
quickplugin <avijitpaul03021992@gmail.com>
2022-07-26 21:49:01 +0530

    card changes

--- styles/components/PersonalCards.style.js
+++ styles/components/PersonalCards.style.js
@@ -10,14 +10,13 @@ const colorPicker = {
 
 
 const PersonalCardContainer = styled.div`
-background-color: ${(props) => colorPicker[props.colorScheme]};
-color: rgba(0, 0, 0, 0.87);
-transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+display: flex;
+background: ${(props)=>colorPicker[props.colorScheme]};
 border-radius: 10px;
-box-shadow: rgb(0 0 0 / 20%) 1px 3px 7px 4px, rgb(0 0 0 / 14%) 0px 1px 1px 0px, rgb(0 0 0 / 12%) 0px 1px 3px 0px;
-overflow: hidden;
+height: 200px;
 width: 100%;
-height:200px;
+color: #ffff;
+box-shadow:1px 6px 10px 5px #aba5a570;
 `
 
 export {
