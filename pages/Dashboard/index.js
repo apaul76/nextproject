daaed6b83f9725d0d99da9c37083e2a298e84433
quickplugin <avijitpaul03021992@gmail.com>
2022-07-27 02:35:24 +0530

    new changes

--- pages/Dashboard/index.js
+++ pages/Dashboard/index.js
@@ -1,16 +1,10 @@
 import PersonalCard from "../../components/PersonalCard";
 import { DashBoardContainer,DashBoardContent} from "../../styles/container/dashboard/dashboard.style";
-import Carousel from "react-material-ui-carousel";
 
 const DashBoard = () => {
     return (
         <DashBoardContainer>
-            <DashBoardContent>
-            <Carousel>
             <PersonalCard />
-            <PersonalCard />
-            </Carousel>
-            </DashBoardContent>
         </DashBoardContainer>
     )
 }
