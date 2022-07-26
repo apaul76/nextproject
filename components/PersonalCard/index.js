7a035e36dd2395a1d6ffd864c83d342a476ea7b8
quickplugin <avijitpaul03021992@gmail.com>
2022-07-26 23:07:29 +0530

    card input

--- components/PersonalCard/index.js
+++ components/PersonalCard/index.js
@@ -1,9 +1,16 @@
 import { PersonalCardContainer } from "../../styles/components/PersonalCards.style";
 
 const PersonalCard = () => {
+    const format = (s) => {
+        return s.toString().replace(/\d{4}(?=.)/g, '$& ');
+    }
     return (
-        <PersonalCardContainer colorScheme="violet">
-            Personal Cards
+        <PersonalCardContainer colorScheme="blue">
+            <ul>
+                <li>AADHAAR</li>
+                <li>{format('515105725637')}</li>
+                <li>Male |  DOB: 03-02-1992</li>
+            </ul>
         </PersonalCardContainer>
     )
 }
--- styles/components/PersonalCards.style.js
+++ styles/components/PersonalCards.style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const colorPicker = {
     orange: '#fe974b',
     violet: '#6752a8',
-    blue:'#7770ff',
+    blue: '#7770ff',
     breeze: '#7ba5ff',
     cremsy: '#f3665d'
 }
@@ -11,12 +11,31 @@ const colorPicker = {
 
 const PersonalCardContainer = styled.div`
 display: flex;
-background: ${(props)=>colorPicker[props.colorScheme]};
-border-radius: 10px;
-height: 200px;
-width: 100%;
-color: #ffff;
-box-shadow:1px 6px 10px 5px #aba5a570;
+background: ${(props) => colorPicker[props.colorScheme]};
+    border-radius: 10px;
+    height: 165px;
+    width: 100%;
+    color: #ffff;
+    box-shadow: 1px 6px 10px 5px #aba5a570;
+    padding: 15px;
+    justify-content: end;
+    & ul {
+        list-style:none;
+        text-align: right;
+        margin: 0px;
+        word-spacing: 4px;
+        letter-spacing: 2px;
+        & li {
+            margin-bottom: 35px;
+        }
+        & li:nth-child(2){
+            font-size: 1.8rem;
+        }
+        & li:nth-child(3){
+            font-size: 0.84rem;
+            margin-top: 2.7rem;
+        }
+    }
 `
 
 export {
