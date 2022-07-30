55e033d3a722be24606cfe164fb1a31a57628701
quickplugin <avijitpaul03021992@gmail.com>
2022-07-30 22:11:58 +0530

    new changes

--- styles/components/PersonalCards.style.js
+++ styles/components/PersonalCards.style.js
@@ -36,6 +36,10 @@ background: ${(props) => colorPicker[props.colorScheme]};
             margin-top: 2.7rem;
         }
     }
+    &:hover{
+        transform: scale(1.05);
+        box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+    }
 `
 
 export {
